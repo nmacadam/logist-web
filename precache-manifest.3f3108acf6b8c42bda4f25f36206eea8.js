f8185dc2a19a832da4b39a5a91999fb7b94208f4
Nathan MacAdam <nkmacadam@gmail.com>
2020-06-06 15:58:17 -0500

    Updates

---
+++ precache-manifest.3f3108acf6b8c42bda4f25f36206eea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8002862075bc70c5c476fae1ba6c71b",
+    "url": "/logist-web/index.html"
+  },
+  {
+    "revision": "76ad96eb90479e476203",
+    "url": "/logist-web/static/css/main.9a6fde90.chunk.css"
+  },
+  {
+    "revision": "0da4715bf0d172b6da26",
+    "url": "/logist-web/static/js/2.35ee1465.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/logist-web/static/js/2.35ee1465.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ad96eb90479e476203",
+    "url": "/logist-web/static/js/main.1a209380.chunk.js"
+  },
+  {
+    "revision": "8abe2eb9b185438bfd80",
+    "url": "/logist-web/static/js/runtime-main.c624d6a3.js"
+  }
+]);
